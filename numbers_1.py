b24a630abf43bd8d4b75ab836435717e8b351f7a
Aldaniyazov <rauanaldaniazov@gmail.com>
2025-01-16 13:11:35 +0500

    Numbers in Python

---
+++ numbers_1.py
@@ -0,0 +1,17 @@
+x = 1
+y = 2.8
+z = 1j
+print(type(x))
+print(type(y))
+print(type(z))
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y)) 
+print(type(z))
+
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
